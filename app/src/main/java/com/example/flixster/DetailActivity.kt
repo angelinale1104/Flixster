1c6211fd144dfd6bf54db8dbf26718666a6143bc
Angelina Le <angelinale1104@gmail.com>
2022-04-03 21:22:33 -0700

    Complete required user stories for part 2

---
+++ app/src/main/java/com/example/flixster/DetailActivity.kt
@@ -0,0 +1,100 @@
+package com.example.flixster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.RatingBar
+import android.widget.TextView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
+import okhttp3.Headers
+
+private const val YOUTUBE_API_KEY = "AIzaSyCenw84BgvP9YCrA_paec0Ynw7SbbhnFiI"
+private const val TRAILERS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
+private const val TAG = "DetailActivity"
+class DetailActivity : YouTubeBaseActivity() {
+
+    private lateinit var tvTitle: TextView
+    private lateinit var tvOverview: TextView
+    private lateinit var ratingBar: RatingBar
+    private lateinit var ytPlayerView: YouTubePlayerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        tvTitle = findViewById(R.id.tvTitle)
+        tvOverview = findViewById(R.id.tvOverview)
+        ratingBar = findViewById(R.id.rbVoteAverage)
+        ytPlayerView = findViewById(R.id.player)
+
+        val movie = intent.getParcelableExtra<Movie>(MOVIE_EXTRA) as Movie
+        Log.i(TAG, "Movie is $movie")
+
+        // Get title of movie
+        tvTitle.text = movie.title
+
+        // Get overview of movie
+        tvOverview.text = movie.overview
+
+        // Get rating of movie
+        ratingBar.rating = movie.voteAverage.toFloat()
+
+        val client = AsyncHttpClient()
+        client.get(TRAILERS_URL.format(movie.movieId), object: JsonHttpResponseHandler() {
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.e(TAG, "onFailure $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                Log.i(TAG, "onSuccess")
+                val results = json.jsonObject.getJSONArray("results")
+                if (results.length() == 0) {
+                    Log.w(TAG, "No movie trailer found!")
+                    return
+                }
+                val movieTrailerJson = results.getJSONObject(0)
+                val youtubeKey = movieTrailerJson.getString("key")
+
+                // PLay youtube video with this trailer only when in onSuccess
+                initializeYoutube(youtubeKey, ratingBar.rating)
+            }
+
+        })
+    }
+
+    private fun initializeYoutube(youtubeKey: String, rating: Float) {
+        ytPlayerView.initialize(YOUTUBE_API_KEY, object: YouTubePlayer.OnInitializedListener {
+            override fun onInitializationSuccess(
+                provider: YouTubePlayer.Provider?,
+                player: YouTubePlayer?,
+                p2: Boolean
+            ) {
+                Log.i(TAG, "onInitializationSuccess")
+                if (rating.compareTo(5.0) > 0) {
+                    // If movie is popular (i.e rating >= 5), play the trailer automatically
+                    player?.loadVideo(youtubeKey)
+                }
+                else {
+                    player?.cueVideo(youtubeKey);
+                }
+            }
+
+            override fun onInitializationFailure(
+                p0: YouTubePlayer.Provider?,
+                p1: YouTubeInitializationResult?
+            ) {
+                Log.i(TAG, "onInitializationFailure")
+            }
+
+        })
+    }
+}
--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,17 +1,24 @@
 package com.example.flixster
 
+import android.app.Activity
+import android.app.ActivityOptions
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
+import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
+import android.util.Pair as UtilPair
 
+
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 private const val TAG = "MovieAdapter"
 
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
@@ -31,14 +38,22 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+        private val playIcon = itemView.findViewById<ImageView>(R.id.icon_play)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
 
         fun bind (movie : Movie) {
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
+
+            // Get rating of movie
+            val rating = movie.voteAverage
 
             val orientation: Int = context.resources.configuration.orientation
             if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
@@ -46,12 +61,38 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
                 Glide.with(context)
                     .load(movie.backdropUrl)
                     .into(ivPoster)
+                if (rating.compareTo(5.0) > 0) {
+                    playIcon.isVisible = true
+                }
             }
             else {
                 // Changed here
                 Glide.with(context)
                     .load(movie.posterImageUrl)
                     .into(ivPoster)
+                if (rating.compareTo(5.0) > 0) {
+                    playIcon.isVisible = true
+                }
+            }
+        }
+
+        override fun onClick(v: View?) {
+            // 1. Get notified of the particular movie which was clicked
+            val movie = movies[adapterPosition]
+
+            // 2. Use the intent system to navigate to new activity
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                // Apply activity transition
+                val options = ActivityOptions.makeSceneTransitionAnimation(context as Activity,
+                    UtilPair.create(tvTitle, "title"),
+                    UtilPair.create(tvOverview, "player"))
+                context.startActivity(intent, options.toBundle())
+            } else {
+                // Swap without transition
+                context.startActivity(intent)
             }
         }
     }
